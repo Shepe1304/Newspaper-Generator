56fa7992125bbfbc52ece3fccf2edbbb143308b3
Quynh-Giang Ho <hqgiang1304@gmail.com>
2023-12-05 03:49:29 +0700

    push smol update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Choices from "./components/layouts/Choices";
 import Footer from "./components/layouts/Footer";
 import Header from "./components/layouts/Header";
 import Main from "./components/pages/Main";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ position: "relative" }}>
       <Header />
+      <Choices />
       <Main />
       <Footer />
     </div>
